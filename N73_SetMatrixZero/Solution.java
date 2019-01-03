5c2fa2f5122a030e99d4df038524de6eb39cb409
Rosie(Ruoxi) Shi <shiruoxi1122a@163.com>
2019-01-03 15:44:51 -0500

    Java-Array-Medium

---
+++ N73_SetMatrixZero/Solution.java
@@ -0,0 +1,35 @@
+package N73_SetMatrixZero;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by srx on 2019/1/3.
+ */
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int R = matrix.length;
+        int C = matrix[0].length;
+        Set<Integer> rows = new HashSet<Integer>();
+        Set<Integer> cols = new HashSet<Integer>();
+
+        // Essentially, we mark the rows and columns that are to be made zero
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    cols.add(j);
+                }
+            }
+        }
+
+        // Iterate over the array once again and using the rows and cols sets, update the elements.
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (rows.contains(i) || cols.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
