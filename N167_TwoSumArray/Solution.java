9405a3b7237eed21d34e2ae7ad56a3a0d1b62a72
Rosie(Ruoxi) Shi <shiruoxi1122a@163.com>
2019-01-02 22:22:23 -0500

    Java-Array-Easy

---
+++ N167_TwoSumArray/Solution.java
@@ -0,0 +1,20 @@
+package N167_TwoSumArray;
+
+/**
+ * Created by srx on 2019/1/2.
+ */
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int i = 0;
+        int j = numbers.length-1;
+        while(i<j){
+            if (numbers[i]+numbers[j]==target)
+                return new int[]{i+1,j+1};
+            else if (numbers[i]+numbers[j]<target)
+                i++;
+            else
+                j--;
+        }
+        return null;
+    }
+}
