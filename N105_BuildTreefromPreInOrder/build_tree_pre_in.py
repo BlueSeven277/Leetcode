7935c31c255b2fd39e0d80dd91b843d036a81472
Rosie(Ruoxi) Shi <shiruoxi1122a@163.com>
2019-01-11 14:02:11 -0500

    Python-Tree/Recursion-Medium

---
+++ N105_BuildTreefromPreInOrder/build_tree_pre_in.py
@@ -0,0 +1,10 @@
+from N94 import TreeNode
+
+
+def buildTree(self, preorder, inorder):
+    if inorder:
+        ind = inorder.index(preorder.pop(0))
+        root = TreeNode(inorder[ind])
+        root.left = self.buildTree(preorder, inorder[0:ind])
+        root.right = self.buildTree(preorder, inorder[ind + 1:])
+        return root
